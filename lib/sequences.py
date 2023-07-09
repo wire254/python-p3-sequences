668faa3d8e810fd2a6625e02da0a7cc981d12a8e
Ulises <ulises.argueta95@gmail.com>
2023-07-09 11:28:16 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    x = 0
+    y = 1
+    z = []
+    for i in range(length):
+        z.append(x)
+        x, y = y, x + y
+    
+    print(z)
+    
